45ad489f7469decaf627dc6aa335007c3d924586
varvar <varvar@yoba.com>
2025-04-07 22:57:19 +0300

    Post model

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,7 +15,6 @@ dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 
-//    implementation("org.springframework:springframework:6.2.1")
     implementation("org.springframework:spring-webmvc:6.2.1")
     implementation("org.thymeleaf:thymeleaf:3.1.2.RELEASE")
     implementation("org.thymeleaf:thymeleaf-spring6:3.1.2.RELEASE")
