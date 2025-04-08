599d2ecba877763edc7c646b2a306ff2cbd6bebb
varvar <varvar@yoba.com>
2025-04-09 00:27:33 +0300

    Rough PostController method sketches based on html-template

--- src/main/java/org/example/controller/PostController.java
+++ src/main/java/org/example/controller/PostController.java
@@ -1,4 +1,27 @@
 package org.example.controller;
 
+import org.springframework.stereotype.Controller;
+
+@Controller
 public class PostController {
+
+    public String getPosts() {}
+
+    public String getPost() {}
+
+    public String addPost() {}
+
+    public String getImage() {}
+
+    public String updatePost() {}
+
+    public String likePost() {}
+
+    public String addComment() {}
+
+    public String updateComment() {}
+
+    public String deleteComment() {}
+
+    public String deletePost() {}
 }
