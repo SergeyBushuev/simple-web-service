9906663bbb7aacd2020d519654f5a6236a7c41fc
varvar <varvar@yoba.com>
2025-04-09 23:07:00 +0300

    Less-less rought PostController method sketches based on html-template

--- src/main/java/org/example/controller/PostController.java
+++ src/main/java/org/example/controller/PostController.java
@@ -4,49 +4,90 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
 @Controller
 public class PostController {
 
     @GetMapping("/")
-    public String getPostsRedirect() {}
+    public String getPostsRedirect() {
+        return "redirect:/posts";
+    }
 
     @GetMapping("/posts")
-    public String getPosts(String search, int pageSize, int pageNumber) {}
+    public String getPosts(@RequestParam(value = "search", defaultValue = "") String search,
+                           @RequestParam(value = "pageSize", defaultValue = "10") int pageSize,
+                           @RequestParam(value = "pageNumber", defaultValue = "1") int pageNumber) {
+        return "posts";
+    }
 
     @GetMapping("/posts{id}")
-    public String getPost(int id) {}
+    public String getPost(@RequestParam Long id) {
+        return "post";
+    }
 
     @GetMapping("/posts/add")
-    public String addPostPage() {}
+    public String addPostPage() {
+        return "add-post";
+    }
 
     @PostMapping("/posts")
-    public String addPost(String title, String text, MultipartFile image, String tags) {}
+    public String addPost(@RequestParam(value = "title") String title,
+                          @RequestParam(value = "text") String text,
+                          @RequestParam(value = "image") MultipartFile image,
+                          @RequestParam(value = "tags") String tags) {
+
+        return "redirect:/posts/" /* TODO +postID */;
+    }
 
     @PostMapping("/posts/{id}/edit")
-    public String editPostPage(int id) {}
+    public String editPostPage(@RequestParam Long id) {
+        return "redirect:/posts/" + id;
+    }
 
     @PostMapping("/posts/{id}")
-    public String editPost(int id, String title, String text, MultipartFile image, String tags) {}
+    public String editPost(@RequestParam Long id,
+                           @RequestParam(value = "title") String title,
+                           @RequestParam(value = "text") String text,
+                           @RequestParam(value = "image") MultipartFile image,
+                           @RequestParam(value = "tags") String tags) {
+        return "redirect:/posts/" + id;
+    }
 
     @PostMapping("/posts/{id}/like")
-    public String likePost(int id, boolean like) {}
+    public String likePost(@RequestParam Long id,
+                           @RequestParam(value = "like") boolean like) {
+        return "redirect:/posts/" + id;
+    }
 
     @PostMapping("/posts/{id}/delete")
-    public String deletePost(int id) {}
+    public String deletePost(@RequestParam Long id) {
+        return "redirect:/posts/";
+    }
 
     @PostMapping("/posts/{id}/comments")
-    public String addComment(int id, String text) {}
+    public String addComment(@RequestParam Long id,
+                             @RequestParam(value = "text") String text) {
+        return "redirect:/posts/" + id;
+    }
 
     @PostMapping("/posts/{id}/comments/{commentId}")
-    public String editComment(int id, int commentId, String text) {}
+    public String editComment(@RequestParam Long id,
+                              @RequestParam(value = "commentId") int commentId,
+                              @RequestParam(value = "tex") String text) {
+        return "redirect:/posts/" + id;
+    }
 
     @PostMapping("/posts/{id}/comments/{commentId}/delete")
-    public String deleteComment(int id, int commentId) {}
+    public String deleteComment(@RequestParam Long id,
+                                @RequestParam(value = "commentId") int commentId) {
+        return "redirect:/posts" + id;
+    }
 
     @GetMapping("/images/{id}")
-    public String getImage(int id) {}
-
+    public String getImage(@RequestParam Long id) {
+        return null; /* TODO image load*/
+    }
 
 }
