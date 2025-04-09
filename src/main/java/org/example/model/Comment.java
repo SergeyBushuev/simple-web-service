c5d3a2c14fc222f3d8950fc7e3d33d3203801710
varvar <varvar@yoba.com>
2025-04-10 00:52:54 +0300

    Comment model

---
+++ src/main/java/org/example/model/Comment.java
@@ -0,0 +1,40 @@
+package org.example.model;
+
+public class Comment {
+    private Long id;
+    private Long postId;
+    private String text;
+
+    public Comment() {
+    }
+
+    public Comment(Long id, Long postId, String text) {
+        this.id = id;
+        this.postId = postId;
+        this.text = text;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getPostId() {
+        return postId;
+    }
+
+    public void setPostId(Long postId) {
+        this.postId = postId;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+}
