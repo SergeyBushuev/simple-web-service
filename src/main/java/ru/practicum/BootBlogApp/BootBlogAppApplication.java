54252f1e4c3c3d5c8bf49425a25a6785df5d78ed
varvar <varvar@yoba.com>
2025-04-18 14:03:55 +0300

    mvn + springboot initial

---
+++ src/main/java/ru/practicum/BootBlogApp/BootBlogAppApplication.java
@@ -0,0 +1,13 @@
+package ru.practicum.BootBlogApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootBlogAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootBlogAppApplication.class, args);
+	}
+
+}
